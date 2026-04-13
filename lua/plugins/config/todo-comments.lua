2d26422c5589c98e9051ba7fce06d32654e71ab3
andyvb1 <andyvanbelzen@gmail.com>
2026-04-13 16:35:37 +0200

    Update regex patterns for todo comments highlighting

--- lua/plugins/config/todo-comments.lua
+++ lua/plugins/config/todo-comments.lua
@@ -30,10 +30,10 @@ return function()
 			multiline = true, -- enable multine todo comments
 			multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
 			multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-			before = "", -- "fg" or "bg" or empty
+			before = "fg", -- "fg" or "bg" or empty
 			keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
 			after = "fg", -- "fg" or "bg" or empty
-			pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+		    pattern = [[\v.*<(%(\d\d-)?(KEYWORDS)%(\(.{-1,}\))?(-\d\d)?):]], -- pattern or table of patterns, used for highlighting (vim regex)
 			comments_only = true, -- uses treesitter to match keywords in comments only
 			max_line_len = 400, -- ignore lines longer than this
 			exclude = {}, -- list of file types to exclude highlighting
@@ -59,7 +59,7 @@ return function()
 			},
 			-- regex that will be used to match keywords.
 			-- don't replace the (KEYWORDS) placeholder
-			pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+			pattern = [[(\d\d-)?(KEYWORDS)(\(\w*\))*(-\d\d)?:]], -- ripgrep regex
 			-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
 		},
 	})
