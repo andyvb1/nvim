c4a194550498d3188d3a55ecd3c6dcd7fbccaeb4
andyvb0 <andyvanbelzen@gmail.com>
2026-04-15 10:30:05 +0200

    TMP: persistent toggle conform save on format

--- lua/plugins/config/conform.lua
+++ lua/plugins/config/conform.lua
@@ -1,4 +1,8 @@
 return function()
+	if vim.g.CONFORM_FORMAT_ON_SAVE == nil then
+		vim.g.CONFORM_FORMAT_ON_SAVE = true
+	end
+
 	require("conform").setup({
 		formatters_by_ft = {
 			lua = { "stylua" },
@@ -13,10 +17,35 @@ return function()
 				prepend_args = { "--aosp" },
 			},
 		},
-		-- Optional: Enable format-on-save
+		--[[ Optional: Enable format-on-save
 		format_on_save = {
 			timeout_ms = 500,
 			lsp_format = "fallback",
-		},
+		} ,]]
+		format_on_save = function(bufnr)
+			if not vim.g.CONFORM_FORMAT_ON_SAVE then
+				return nil
+			end
+			return {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			}
+		end,
 	})
+	local function set_format_on_save(enabled)
+		vim.g.CONFORM_FORMAT_ON_SAVE = enabled
+		vim.notify("Format on save: " .. (enabled and "ON" or "OFF"), vim.log.levels.INFO)
+	end
+
+	vim.api.nvim_create_user_command("ConformAutoFormatToggle", function()
+		set_format_on_save(not vim.g.CONFORM_FORMAT_ON_SAVE)
+	end, { desc = "Toggle conform format on save" })
+
+	vim.api.nvim_create_user_command("ConformAutoFormatOn", function()
+		set_format_on_save(true)
+	end, { desc = "Enable conform format on save" })
+
+	vim.api.nvim_create_user_command("ConformAutoFormatOff", function()
+		set_format_on_save(false)
+	end, { desc = "Disable conform format on save" })
 end
